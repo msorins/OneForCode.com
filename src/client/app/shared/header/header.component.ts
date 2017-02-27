718ab92cb8a2352e279b32295d48abf0ec3d9120
Sorin Sebastian Mircea <sorynsoo@gmail.com>
2017-02-27 19:30:42 +0200

    Auto transitioning between image headers every 30s

--- src/client/app/shared/header/header.component.ts
+++ src/client/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs/Rx";
+
 
 /**
  * This class represents the header component.
@@ -10,6 +12,32 @@ import { Component } from '@angular/core';
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.css']
 })
-export class HeaderComponent{
+export class HeaderComponent implements OnInit {
+    public imgNumber:number = 1;
+    public imgMaxNumber:number = 6;
+    public showState = true;
 
+    ngOnInit() {
+      //Call image transition every 30 seconds
+      Observable.interval(30000).subscribe(x => {
+        this.changePicture()
+      });
+    }
+
+    changePicture() {
+      //Initiate the transition
+      this.showState = false;
+
+      //Chose the new image (random)
+      let newImgNumber = Math.floor(Math.random() * 6) + 1;
+      while(this.imgNumber != newImgNumber)
+        this.imgNumber = Math.floor(Math.random() * 6) + 1;
+
+      //Complete the transition after 300
+      let finishTransition = Observable.interval(450);
+      finishTransition.first().subscribe( x => {
+          this.showState = true;
+        })
+
+    }
 }
